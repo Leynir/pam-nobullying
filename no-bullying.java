8443b1941e5916524cc9f46b4047c887a0fbd716
LENILSON EDUARDO DE LIMA SANTOS <415543204@alunos.etec.itaqua>
2019-03-14 20:32:02 -0300

    adicionado os 10 passos

--- no-bullying.java
+++ no-bullying.java
@@ -3,3 +3,16 @@ public static void main(String[] args){
  System.out.println('Programa NoBullying'); 
 	} 
  } 
+	System.out.println("Passo 1: Verificar o jeito do aluno se ele está isolado "); 
+	System.out.println("Passo 2: Ver como as pessoas tratam ele"); 
+	System.out.println("Passo 3: Se aproximar da pessoa "); 
+	System.out.println("Passo 4: Se colocar no lugar da pessoa"); 
+	System.out.println("Passo 5: Se for na escola conversar com o professor sobre o aluno para fornecer um auxilio"); 
+	System.out.println("Passo 6: Conversar com os pais sobre o comportamento da pessoa"); 
+	System.out.println("Passo 7: Procurar por um auxilio psicologico"); 
+	System.out.println("Passo 8: Encaminha a pessoa ao centro de apoio");
+	System.out.println("Passo 9: Conversar com a pessoa que pratica o bullying pois as vezes ele também pode ter algum problema e disconta isso nas outras pessoas ");
+	System.out.println("Passo 10: Ter mais auxilio psicologico nas escolas");
+	
+	
+	
